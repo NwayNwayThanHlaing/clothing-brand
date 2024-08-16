57c840452c4629a18be4331cc0f2102077e5fe6d
Nway Nway Than Hlaing <wlis130@live.rhul.ac.uk>
2024-08-16 16:23:56 +0100

    added photos for women wear and created nav-bar with dropdowns.

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -14,8 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <Navbar className="" />
+        <div className="">{children}</div>
         <Footer />
       </body>
     </html>

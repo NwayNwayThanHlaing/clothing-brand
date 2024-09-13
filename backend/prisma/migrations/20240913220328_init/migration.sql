c331270c08af0fce419d457a62a842982ef2323a
Nway Nway Than Hlaing <wlis130@live.rhul.ac.uk>
2024-09-13 23:07:45 +0100

    resolved prisma installation error

--- backend/prisma/migrations/20240913220328_init/migration.sql
+++ backend/prisma/migrations/20240913220328_init/migration.sql
@@ -1,28 +1,14 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Profile` table. If the table is not empty, all the data it contains will be lost.
-  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
-  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- DropForeignKey
-ALTER TABLE `Post` DROP FOREIGN KEY `Post_authorId_fkey`;
-
--- DropForeignKey
-ALTER TABLE `Profile` DROP FOREIGN KEY `Profile_userId_fkey`;
-
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    ADD COLUMN `password` VARCHAR(191) NOT NULL,
-    MODIFY `name` VARCHAR(191) NOT NULL;
-
--- DropTable
-DROP TABLE `Post`;
-
--- DropTable
-DROP TABLE `Profile`;
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Admin` (
